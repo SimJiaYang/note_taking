cfe3a79c92eb08d63a6aa40e0df61c017cada919
Louis Sim <B210094B@sc.edu.my>
2023-04-05 23:56:33 +0800

    CRUD Completed

--- lib/screens/note_reader.dart
+++ lib/screens/note_reader.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_taking/style/app_style.dart';
+import 'package:quickalert/quickalert.dart';
 
 class NoteReaderScreen extends StatefulWidget {
   NoteReaderScreen(this.doc, {Key? key}) : super(key: key);
@@ -26,10 +27,44 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
 
     return Scaffold(
       backgroundColor: colour,
-      appBar: AppBar(
-        backgroundColor: AppStyle.mainColor,
-        elevation: 0.0,
-      ),
+
+        appBar: AppBar(
+          backgroundColor: AppStyle.mainColor,
+          title: const Text('Note'),
+          actions: <Widget>[
+
+            IconButton(
+              icon: const Icon(Icons.delete),
+              tooltip: 'Go to the next page',
+              onPressed: () async {
+                QuickAlert.show(
+                  context: context,
+                  type: QuickAlertType.confirm,
+                  text: 'Do you want to logout',
+                  confirmBtnText: 'Yes',
+                  cancelBtnText: 'No',
+                  confirmBtnColor: Colors.green,
+
+                  onConfirmBtnTap: () async {
+                    await FirebaseFirestore.instance.
+                        collection("Notes")
+                        .doc(docID)
+                        .delete()
+                        .then((_) => print('Deleted'))
+                        .catchError((error) => print('Delete failed: $error'));
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  }
+                  );
+
+              },
+            ),
+          ],
+        ),
+
+
+      //   elevation: 0.0,
+      // ),
       body:SafeArea(
         child: Padding(
           padding: EdgeInsets.all(16.0),
@@ -38,7 +73,7 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
           children: <Widget>[
 
             Expanded(
-              flex: 1,
+              flex: 0,
               child: TextField(
                 controller: _titleControlller,
                 keyboardType: TextInputType.multiline,
@@ -52,13 +87,14 @@ class _NoteReaderScreenState extends State<NoteReaderScreen> {
             
 
             Expanded(
-                flex: 1,
+                flex:0,
                 child: Text(widget.doc["create_date"],style: AppStyle.dateTitle,)
             ),
 
             Expanded(
               flex: 4,
               child: TextField(
+                textAlign:TextAlign.justify,
                 controller: _mainControlller,
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
